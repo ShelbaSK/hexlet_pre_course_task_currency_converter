a59871f3ed7a8535f2954a5ebdc8938645804735
ShelbaSK <49717114+ShelbaSK@users.noreply.github.com>
2023-10-21 11:14:46 +0700

    ver 0.1

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 import requests
 
-URL = 'https://api.freecurrencyapi.com/v1/latest?apikey='
-URL_PARAMS = 'currencies='
-API_KEY = 'fca_live_mvzs5GSll5nO70RxrnU3L2TOE1DYOoxfcYoMG4tD'
 
-
-def get_ticket(url_req):
+def get_tickets(url_req):
     result = requests.get(url_req)
     return result.json()
 
 
-def print_available_ticket(lib, count_tickets_in_line):
-    count = 0
+def get_available_tickets(lib, count_tickets_in_line):
+    lib_keys = list(lib.keys())
     result = ''
-    for ticket in lib:
-        if result == '':
-            result = ticket
-        else:
-            result += ', ' + ticket
-        count += 1
-        if count >= count_tickets_in_line:
-            print(result)
-            count = 0
-            result = ''
-    if result != '':
-        print(result)
+    while len(lib_keys) > count_tickets_in_line:
+        result += ', '.join(lib_keys[:count_tickets_in_line]) + '\n'
+        for keys in lib_keys[:count_tickets_in_line]:
+            lib_keys.remove(keys)
+    result += ', '.join(lib_keys)
+    return result
 
 
 def choose_ticket(message, lib):
     count_try = 0
     ticket = input(message).strip()
-    while count_try <= 10:
+    while count_try <= 9:
         if ticket in lib:
             return ticket
         else:
             ticket = input('Указанная валюта не найдена, попробуйте еще раз: ').strip()
+        count_try += 1
+    print('My job here is done!')
+    exit('Превышено количество попыток ввода')
+
+
+def entering_number(message, count_round):
+    count_try = 0
+    number = input(message).strip()
+    while count_try < 9:
+        try:
+            result = round(float(number), count_round)
+            return result
+        except:
+            number = input('Введеное значение должно быть числом, попробуйте еще раз: ').strip()
         count_try += 1
     print('My job here is done!')
     exit('Превышено количество попыток ввода')
@@ -46,7 +50,11 @@ def convert_ticket(from_currency, to_currency, amount_currency, lib):
     return result
 
 
-tickets_lib = get_ticket(f'{URL}{API_KEY}&{URL_PARAMS}')['data']
+URL = 'https://api.freecurrencyapi.com/v1/latest?apikey='
+URL_PARAMS = 'currencies='
+API_KEY = 'fca_live_mvzs5GSll5nO70RxrnU3L2TOE1DYOoxfcYoMG4tD'
+
+tickets_lib = get_tickets(f'{URL}{API_KEY}&{URL_PARAMS}')['data']
 
 print(f'''Конвертер валют
 
@@ -57,12 +65,12 @@ print(f'''Конвертер валют
 
 Доступные валюты:''')
 
-print_available_ticket(tickets_lib, 10)
+print(get_available_tickets(tickets_lib, 10))
 print()
 
 from_ticket = choose_ticket('Введите исходную валюту: ', tickets_lib)
 to_ticket = choose_ticket('Введите конечную валюту: ', tickets_lib)
-amount = float(input('Введите количество исходной валюты: ').strip())
+amount = entering_number('Введите количество исходной валюты: ', 2)
 
 converted_amount = round(convert_ticket(from_ticket, to_ticket, amount, tickets_lib), 2)
 
